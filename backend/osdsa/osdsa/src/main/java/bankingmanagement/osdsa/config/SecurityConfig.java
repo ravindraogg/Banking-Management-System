7ca515ca47d9a20ee6415e3d2ec432051748952c
ravindraogg <ravindraravindra7627@gmail.com>
2024-12-21 09:02:42 +0530

    deploy

--- backend/osdsa/osdsa/src/main/java/bankingmanagement/osdsa/config/SecurityConfig.java
+++ backend/osdsa/osdsa/src/main/java/bankingmanagement/osdsa/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://pluto-banking.netlify.app");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
