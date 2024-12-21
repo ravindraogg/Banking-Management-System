7ad2cfb9626710919339f8adc4b30932c14820d2
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-12-21 08:44:34 +0530

    Update MainHeader.jsx

--- frontend/banking-management/src/pages/MainHeader.jsx
+++ frontend/banking-management/src/pages/MainHeader.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const MainHeader = () => {
   return (
     <header className="bg-blue-600 text-white shadow-md p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">Pluto Banking</h1>
+ <Link to="/">
+      <h1 className="text-2xl font-bold">Pluto Banking</h1></Link>
       
       <Link to="/login">
         <button className="bg-white text-blue-600 px-4 py-2 rounded shadow hover:bg-gray-200">
