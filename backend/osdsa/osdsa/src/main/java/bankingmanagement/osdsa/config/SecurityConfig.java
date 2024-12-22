bbbe2d5da7db8470dca2d1c9b2092eb757a29128
ravindraogg <ravindraravindra7627@gmail.com>
2024-12-22 12:38:40 +0530

    added backend url

--- backend/osdsa/osdsa/src/main/java/bankingmanagement/osdsa/config/SecurityConfig.java
+++ backend/osdsa/osdsa/src/main/java/bankingmanagement/osdsa/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("https://pluto-banking.netlify.app");
+        config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
