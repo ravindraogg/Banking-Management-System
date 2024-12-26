431acc2a72381298185be271825308f97eddfe28
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-12-26 21:50:53 +0530

    Update Home.jsx

--- frontend/banking-management/src/pages/Home.jsx
+++ frontend/banking-management/src/pages/Home.jsx
@@ -60,15 +60,16 @@ const Home = () => {
             </p>
           </div>
         </section>
-        <footer className="bg-gray-900 text-white p-4 text-center">
-      <p className="text-sm">
-        &copy; {new Date().getFullYear()} Banking App. All rights reserved.
-      </p>
-      <p className="text-sm mt-2">
-        Developed by: Ravindra S, Nitesh Reddy, Pooja C.G, Bhavana, and Dhanushree.
-      </p>
-    </footer>
       </div>
+      {/* Footer Section */}
+      <footer className="bg-gray-900 text-white p-4 text-center w-full">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} Banking App. All rights reserved.
+        </p>
+        <p className="text-sm mt-2">
+          Developed by: Ravindra S, Nitesh Reddy, Pooja C.G, Bhavana, and Dhanushree.
+        </p>
+      </footer>
     </>
   );
 };
